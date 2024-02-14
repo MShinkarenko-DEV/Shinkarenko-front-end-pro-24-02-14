475d518934d4b6965d621819d18ea240837252f4
Mikhail <father.my.nyashka@gmail.com>
2024-02-14 23:23:30 +0200

    task 1 is done

---
+++ HW2/task1/main.js
@@ -0,0 +1,5 @@
+const firstLine = prompt("Please input 1st line");
+const secondLine = prompt("Please input 2nd line");
+const thirdLine = prompt("Please input 3rd line");
+const commonLine = firstLine + ' ' +  secondLine + ' ' + thirdLine;
+alert(commonLine);
--- HW2/task2/main.js
+++ HW2/task2/main.js
@@ -1,4 +1,3 @@
-console.log("in main js");
 let num = prompt("Please input 5 digits number");
 console.log(num);
 alert(`${num[0]} ${num[1]} ${num[2]} ${num[3]} ${num[4]}`);
